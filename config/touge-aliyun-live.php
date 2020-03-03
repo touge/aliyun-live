ce3bcb10ab7096d7742fa2b88415ec4106856b09
NickYuan <nick@NickYuandeMacBook-Pro.local>
2020-03-03 19:40:01 +0800

    update

---
+++ config/touge-aliyun-live.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nick
+ * Date: 2018/8/29
+ * Time: 上午11:07
+ */
+return [
+    'accessKeyId'=> '*',
+    'accessKeySecret'=> '*',
+
+
+    /**
+     * 域名配置
+     */
+    'domain'=> [
+        'pull'=> [
+            'auth_key'=> '*',
+            'url'=> '*',
+            'expire'=> 10800, //三个小时
+        ],
+        'push'=> [
+            'auth_key'=> '*',
+            'url'=> '*',
+            'expire'=> 10800,
+        ]
+    ],
+];
